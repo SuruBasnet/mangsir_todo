0fe7679852e06e1a79c9eb245f0030da9231c103
SuruBasnet <surubasnet824@gmail.com>
2024-01-01 08:39:10 +0545

    data edit and delete done

--- Todo/urls.py
+++ Todo/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from base.views import home,create,edit
+from base.views import home,create,edit,delete
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",home, name='home'),
     path("create/",create,name='create'),
-    path("edit/<int:pk>/",edit,name='edit')
+    path("edit/<int:pk>/",edit,name='edit'),
+    path("delete/<int:pk>/",delete,name='delete'),
 ]
--- base/views.py
+++ base/views.py
@@ -20,6 +20,19 @@ def create(request):
 
 def edit(request,pk):
     todo = Todo.objects.get(id=pk)
-    print(todo)
-    content = {'mode':'edit'}
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        todo.name = name
+        todo.description = description
+        todo.status = status
+        todo.save()
+        return redirect('home')
+    content = {'mode':'edit','todo':todo}
     return render(request,'create.html',context=content)
+
+def delete(request,pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect('home')
